7fb85ba09c08dd434f436b5df779ac101b144229
jeannoel.camesi <jean-noel.camesi@etu.hesge.ch>
2020-09-01 23:18:23 +0200

    .

---
+++ server.js
@@ -0,0 +1,35 @@
+const port = 8080;
+
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const compression = require("compression");
+
+const server = express();
+
+server.set("view options", {layout: false});
+server.engine("html", ejs.renderFile);
+server.set("view engine", "html");
+server.set("views", __dirname + "/public");
+
+server.get("/", (req, res) => {
+	res.render("index", {
+		title: "Index"
+	});
+});
+
+server.get("/subpage", (req, res) => {
+	res.render("subpage", {
+		title: "Subpage"
+	});
+});
+
+
+server.use(compression());
+server.use(express.static(path.join(__dirname, 'public')), (req, res) => {
+	res.redirect("/");
+});
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}/`);
+});
